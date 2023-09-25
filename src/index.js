ccd6f728cc655cbf0c33bdc544bab612f585d736
lipa88-mk <lipa88@mail.ru>
2023-09-25 22:13:33 +0400

    init

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/style.scss";
-import random from 'lodash/random'
-import _ from 'lodash'
-
-console.log('hi webpack')
-console.log(random(0,10))
-console.log(random(0,100))
-console.log(_.partition([1, 2, 3, 4], n => n % 2))
